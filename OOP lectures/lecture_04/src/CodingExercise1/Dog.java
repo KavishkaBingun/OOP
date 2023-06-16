a6706286b12e62a4884f8d796a38cc657e143727
Kavishka Bingun Sathsarani <108647821+KavishkaBingun@users.noreply.github.com>
2023-06-16 12:11:29 +0530

    Delete OOP lectures/lecture_04 directory

--- OOP lectures/lecture_04/src/CodingExercise1/Dog.java
+++
@@ -1,9 +0,0 @@
-package CodingExercise1;
-
-public class Dog extends Animal {
-	
-	public void makeSound() {
-		System.out.println("Woof Woof\n");
-	}
-
-}
--- OOP lectures/lecture_04/src/CodingExercise2/Printable.java
+++
@@ -1,9 +0,0 @@
-package CodingExercise2;
-
-public interface Printable {/* if all the methods in a class are
-abstract methos that is not a class that is interface*/
-	
-	public void print();/*this is the only methoth in printable
-	and it also abstract method*/
-
-}
